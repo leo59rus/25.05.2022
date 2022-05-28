013cea1d71056cca7cae4f306fb6d4eb38e33d57
HP <orange59rus@mail.ru>
2022-05-28 14:12:15 +0300

    hw1 начало

---
+++ src/main/java/hw1_level2/Human.java
@@ -0,0 +1,13 @@
+package hw1_level2;
+
+class Human implements Ready{
+    @Override
+    public void run(int length) {
+
+    }
+
+    @Override
+    public void jump(int height) {
+
+    }
+}
---
+++ src/main/java/hw1_level2/Ready.java
@@ -0,0 +1,7 @@
+package hw1_level2;
+
+public interface Ready {
+    void run(int length);
+
+    void jump(int height);
+}
