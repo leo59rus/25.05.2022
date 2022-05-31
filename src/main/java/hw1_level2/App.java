8a3f4a4503e1957f8c96fca4910f9224318c4614
HP <orange59rus@mail.ru>
2022-05-31 19:39:26 +0300

    hw1 не разобрался(

--- src/main/java/hw1_level2/App.java
+++ src/main/java/hw1_level2/App.java
@@ -13,34 +13,36 @@ package hw1_level2;
 //препятствий не идет.
 public class App {
     public static void main(String[] args) {
-        Cat cat1 = new Cat("Пушок");
-        Human human1 = new Human("Сильвестер");
-        Robot robot1 = new Robot("SI-corp 5.2");
-        System.out.println(cat1.toString());
-        System.out.println(human1.toString());
-        System.out.println(robot1.toString());
-        cat1.run(0.2);
-        cat1.runTrack(0.2);
-        cat1.jump(25);
-        cat1.jumpWall(25);
+     //  Cat cat1 = new Cat("Пушок");
+     //  Human human1 = new Human("Сильвестер");
+     //  Robot robot1 = new Robot("SI-corp 5.2");
+     //  System.out.println(cat1.toString());
+     //  System.out.println(human1.toString());
+     //  System.out.println(robot1.toString());
+     //  cat1.run(0.2);
+     //  cat1.runTrack(0.2);
+     //  cat1.jump(25);
+     //  cat1.jumpWall(25);
 
-        human1.run(10);
-        human1.runTrack(10);
-        human1.jump(120);
-        human1.jumpWall(120);
+     //  human1.run(10);
+     //  human1.runTrack(10);
+     //  human1.jump(120);
+     //  human1.jumpWall(120);
 
-        robot1.run(29);
-        robot1.runTrack(29);
-        robot1.jump(5000);
-        robot1.jumpWall(5000);
+     //  robot1.run(29);
+     //  robot1.runTrack(29);
+     //  robot1.jump(5000);
+     //  robot1.jumpWall(5000);
 
-       // String[] athlets = {
-       //         String.valueOf(cat1),
-       //         String.valueOf(human1),
-       //         String.valueOf(robot1)
-       // };
-       // for (String athlet : athlets) {
-       //     if (athlet.getLengthRun(
-       // }
+      Ready[] go = new Ready[6];
+      go[0] = new Android();
+      go[1] = new HumanMan();
+      go[2] = new CatMeow();
+      go[3] = new Robot("SI-corp 5.2");
+      go[4] = new Human("Andrei");
+      go[5] = new Cat("Meow");
+
+
+
     }
 }
---
+++ src/main/java/hw1_level2/HumanMan.java
@@ -0,0 +1,13 @@
+package hw1_level2;
+
+public class HumanMan implements Ready {
+    @Override
+    public void run(double length) {
+
+    }
+
+    @Override
+    public void jump(int height) {
+
+    }
+}
