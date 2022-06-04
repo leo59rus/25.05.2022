889e9a9fa1fb5d0f1c2610541cd61005990ecaba
HP <orange59rus@mail.ru>
2022-06-04 16:07:58 +0300

    домашняя работа 2, уровень 2

---
+++ src/main/java/homeWork_2_level2/Main.java
@@ -0,0 +1,57 @@
+package homeWork_2_level2;
+//1. Напишите метод, на вход которого подаётся двумерный строковый массив размером 4х4,
+// при подаче массива другого размера необходимо бросить исключение MyArraySizeException.
+//2. Далее метод должен пройтись по всем элементам массива, преобразовать в int, и просуммировать.
+// Если в каком-то элементе массива преобразование не удалось (например, в ячейке лежит символ или текст вместо числа),
+// должно быть брошено исключение MyArrayDataException, с детализацией в какой именно ячейке лежат неверные данные.
+//3. В методе main() вызвать полученный метод, обработать возможные исключения MySizeArrayException
+// и MyArrayDataException, и вывести результат расчета.
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[][] massiv = new String[][]{
+                {"1", "1", "1", "1"},
+                {"1", "1", "1", "1"},
+                {"1", "1", "1", "1"},
+                {"1", "1", "1", "1"},
+        };
+        try {
+            try {
+                int result = method(massiv);
+                System.out.println("сумма элементов массива = " + result);
+            } catch (MySizeArrayException e) {
+                System.out.println("Размер массива превышен!");
+            }
+        }
+        catch (MyArrayDataException e) {
+            System.out.println("Неправильное значение массива!");
+            System.out.println("Ошибка в ячейке: " + e.i + "x" + e.j);
+        }
+
+    }
+
+
+    public static int method(String[][] arr) throws MySizeArrayException, MyArrayDataException {
+        int count = 0;
+        if (arr.length != 4) {
+            throw new MySizeArrayException();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != 4) {
+                throw new MySizeArrayException();
+            }
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    count = count + Integer.parseInt(arr[i][j]);
+                }
+                catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+
+        }
+        return count;
+    }
+
+}
---
+++ src/main/java/homeWork_2_level2/MySizeArrayException.java
@@ -0,0 +1,7 @@
+package homeWork_2_level2;
+
+public class MySizeArrayException extends Exception {
+ //MySizeArrayException(String msg) {
+ //    super(msg);
+ //}
+}
