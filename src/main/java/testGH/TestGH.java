8e376b5fbb088865d20b544f9c9ef90ac2e9a3f8
HP <orange59rus@mail.ru>
2022-06-20 20:29:38 +0300

    test GH2

--- src/main/java/testGH/TestGH.java
+++ src/main/java/testGH/TestGH.java
@@ -4,5 +4,6 @@ public class TestGH {
     public static void main(String[] args) {
         System.out.println("test GH");
         System.out.println("test GH2");
+        System.out.println("test GH3");
     }
 }
